6b7f34bf46bf42938bd49e9d8117ec29c3b69efe
Sergei Fomichev <sergei.fomicheff@gmail.com>
2018-04-21 14:35:21 -0400

    added second page

--- react-app/bestbuddies/src/App.js
+++ react-app/bestbuddies/src/App.js
@@ -2,16 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 import BuddyProfile from './BuddyProfile';
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			page: 'buddyList'
+		};
+	}
+	handleGoToBuddyPage() {
+		this.setState({
+			page: 'buddyProfile'
+		})
+	}
+	getPage() {
+		const page = this.state.page;
+		if(page === 'buddyList') {
+			return <BuddyList goToBuddyPage={ handleGoToBuddyPage }/>
+		}
+		return <BuddyProfile />
+	}
+	const page = 'buddylist';
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to BestBuddies</h1>
         </header>
-        <BuddyProfile />
+			{ getPage() }
       </div>
     );
   }
