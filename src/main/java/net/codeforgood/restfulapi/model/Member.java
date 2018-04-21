cbe294931e3f8300c63fee5872b1bb94443a0e61
dimarrik <dimarrika@gmail.com>
2018-04-21 15:45:26 -0400

    init

--- src/main/java/net/codeforgood/restfulapi/model/Member.java
+++ src/main/java/net/codeforgood/restfulapi/model/Member.java
@@ -1,25 +1,53 @@
 package net.codeforgood.restfulapi.model;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
-    private int locationId;
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int memberId;
+    private String memberFname;
+    private String memberLname;
+    private String memberEmail;
+    private Date memberJoinDate;
 
     public Member() {
 
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public int getLocationId() {
-        return locationId;
+    public int getMemberId() {
+        return memberId;
     }
 
-    public void setLocationId(int locationId) {
-        this.locationId = locationId;
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
     }
 
+    public String getMemberFname() {
+        return memberFname;
+    }
 
+    public void setMemberFname(String memberFname) {
+        this.memberFname = memberFname;
+    }
+
+    public String getMemberLname() {
+        return memberLname;
+    }
+
+    public void setMemberLname(String memberLname) {
+        this.memberLname = memberLname;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
 }
--- src/main/java/net/codeforgood/restfulapi/repository/MemeberRepository.java
+++ src/main/java/net/codeforgood/restfulapi/repository/MemeberRepository.java
@@ -3,9 +3,15 @@ package net.codeforgood.restfulapi.repository;
 import net.codeforgood.restfulapi.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemeberRepository extends JpaRepository<Member, Integer>{
 
-//    @Query("select m from member m")
-//    Member getAll();
+    @Query(value = "SELECT * FROM member limit 1", nativeQuery = true)
+    Member getAll();
+
+    @Query(value = "SELECT * FROM member where member_roles =?1", nativeQuery = true)
+    List<Member> memberByRole( int id);
 }
